0eed1bf82b982d08e094ff1082a8ff6aca717e29
Danyel <danyelhenriquefidel@gmail.com>
2020-03-03 23:05:19 -0300

    chore: make aside admin page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
-import Main from "../src/components/Main";
-import Hero from "../src/components/Hero";
 import Nav from "../src/components/Nav";
-import Post from "../src/components/Posts";
-import Contact from "../src/components/Contact";
+import Main from "../src/components/Main";
+
+import Home from "./pages/Home";
+import Admin from "./pages/Admin";
 
 import "../src/assets/css/font-awesome.min.css";
 import "./reset.css";
@@ -11,10 +11,8 @@ import "./reset.css";
 function App() {
   return (
     <Main>
-      <Hero />
       <Nav />
-      <Post />
-      <Contact />
+      <Admin />
     </Main>
   );
 }
